5ae284ad788b571c63f2207975354f774340947b
Joost Roza <joost@ttt.nl>
2022-11-07 19:11:57 +0100

    Add project files.

---
+++ App/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App
+{
+    public class SimpleCustomer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class ComplexCustomer : SimpleCustomer
+    {
+        public Company Company { get; set; }
+    }
+
+    public class Company
+    {
+        public List<SimpleCustomer> Customers { get; set; }
+    }
+}
---
+++ Validators/Class1.cs
@@ -0,0 +1,25 @@
+﻿using App;
+using FluentValidation;
+using System;
+using System.Xml.Schema;
+
+namespace Validators
+{
+    public class SimpleCustomerValidator : AbstractValidator<SimpleCustomer>
+    {
+        public SimpleCustomerValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty();
+        }
+    }
+
+    public class ComplexUserValidator : SimpleCustomerValidator
+    {
+        public ComplexUserValidator()
+            : base()
+        {
+            // some more validation rules
+        }
+    }
+}
